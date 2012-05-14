d07dbfae8ea2feeefc8da3a8c96dfe53d1e9b3c8
Chen Yufei <cyfdecyf@gmail.com>
2012-05-14 10:47:47 +0800

    Add Chinese localization for remove alert sheet.

--- PinYinNick/PYNickAppDelegate.m
+++ PinYinNick/PYNickAppDelegate.m
@@ -179,12 +179,15 @@ static NSString *NICKNAME_IDENTIFIER = @"nickName";
     [_ab save];
 }
 
+#define LocalizedString(key, defaultValue) \
+    [[NSBundle mainBundle] localizedStringForKey:(key) value:defaultValue table:nil]
+
 - (IBAction)removeAllPinyinNickNames:(id)sender {
-    NSAlert *alert = [NSAlert alertWithMessageText:@"Remove all pinyin nick names"
-                                     defaultButton:@"Remove"
-                                   alternateButton:@"Cancel"
+    NSAlert *alert = [NSAlert alertWithMessageText:LocalizedString(@"REMOVE_ALERT_MSG", @"Remove all pinyin nick names")
+                                     defaultButton:LocalizedString(@"REMOVE", @"Remove")
+                                   alternateButton:LocalizedString(@"CANCEL", @"Cancel")
                                        otherButton:nil
-                         informativeTextWithFormat:@"Nick names associated with contacts which have Chinese characters will be deleted."];
+                         informativeTextWithFormat:LocalizedString(@"REMOVE_ALERT_INFO", @"Nick names associated with contacts which have Chinese characters will be deleted!")];
     [alert beginSheetModalForWindow:_window
                       modalDelegate:self
                      didEndSelector:@selector(removeNickAlertEnded:code:context:)
